24e907c6373be4e9be02c27929475a2db5c4a695
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-11-09 11:34:28 +0530

    fix:errorfix

--- sound/wsgi.py
+++ sound/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sound.settings')
 
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
