04fedcd8c34ab391a54f5556fbc7df7d3ac0afc1
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-11-11 08:04:43 +0530

    fix:apt file updated

--- sound/settings.py
+++ sound/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['coughcovidprediction.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    "sslserver",
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
